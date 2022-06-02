f3f5acb28483361ab650b9b7c2d85822c3595845
Gaurav <allmanpride@gmail.com>
2022-06-02 18:25:54 -0400

    Add support for multiple progress bars

--- progress.c
+++ progress.c
@@ -1,10 +1,12 @@
 /*
  * Simple progress bar implementation in C. 
  *
- *
+ * Originally From: https://github.com/c-progress-bar/c-progress-bar
  */
+#include <stddef.h>
 #include <stdio.h>
 #include <math.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/time.h>
@@ -47,6 +49,16 @@ static const char * subprogress_blocks[] = { " ",
 
 #define NUM_SUBBLOCKS (sizeof(subprogress_blocks) / sizeof(subprogress_blocks[0]))
 
+
+struct progress_info {
+  int initialized;
+  int numprocess;
+  char** label;
+  double* percentage;
+  uint64_t* start;
+} global_pi;
+
+
 /* Helper function to get the current time in usecs past the epoch. */
 static uint64_t get_timestamp(void) {
     struct timeval tv;
@@ -87,19 +99,19 @@ static void print_timedelta(uint64_t delta) {
  * start: usecs timestamp for when the task started, for calculating
  *        remaining time.
  */
-void print_progress(double percentage, uint64_t start) {
+void print_single_progress(int num) {
     size_t i;
     size_t total_blocks = PROGRESS_BAR_WIDTH * NUM_SUBBLOCKS;
-    size_t done = round(percentage / 100.0 * total_blocks);
+    size_t done = *(global_pi.percentage + num) / 100 * total_blocks;
     size_t num_blocks = done / NUM_SUBBLOCKS;
     size_t num_subblocks = done % NUM_SUBBLOCKS;
 
     uint64_t now = get_timestamp();
-    uint64_t elapsed = now - start;
-    uint64_t estimated_total = elapsed / (percentage / 100.0);
+    uint64_t elapsed = now - *(global_pi.start + num);
+    uint64_t estimated_total = elapsed / (*(global_pi.percentage + num) / 100.0);
     uint64_t remaining = estimated_total - elapsed;
 
-    printf("   Progress: %6.2f%% \t%s", percentage, BAR_START);
+    printf("\r  %s: %6.2f%% \t%s", *(global_pi.label + num), *(global_pi.percentage + num), BAR_START);
 
     for (i = 0; i < num_blocks; i++) {
         printf("%s", PROGRESS_BLOCK);
@@ -117,26 +129,72 @@ void print_progress(double percentage, uint64_t start) {
     
     printf("%s\t", BAR_STOP);
 
-    if (percentage < 100.0) {
+    if (*(global_pi.percentage + num) < 100.0) {
         printf("ETA: ");
         print_timedelta(remaining);
     }
     else {
-        printf("                          ");
+        printf("\x1b[K"); 	/* delete till end of line */
     }
-    printf("\r");
     fflush(stdout);
 }
+
+
+void print_multiple_progress();
+
+void init_progress_bars(int num){
+  global_pi.initialized = 0;
+  global_pi.numprocess = num;
+  global_pi.label = malloc(sizeof(char*)*num);
+  global_pi.percentage = malloc(sizeof(double*)*num);
+  global_pi.start = malloc(sizeof(uint64_t*)*num);
+}
+
+void set_progress_bar(int index, char* label, double percentage, uint64_t start){
+  *(global_pi.label + index) = label;
+  *(global_pi.percentage + index) = percentage;
+  *(global_pi.start + index) = start;
+}
+
+void update_progress_bar(int index, double percentage){
+  *(global_pi.percentage + index) = percentage;
+}
+
+void free_progress_bars(int num){
+  global_pi.numprocess = 0;
+  free(global_pi.label);
+  free(global_pi.percentage);
+  free(global_pi.start);
+}
+
+void print_multiple_progress(){
+  int i;
+  if (global_pi.initialized){
+    /* goto previous lines */
+    printf("\x1b[%dA", global_pi.numprocess);
+  }else{
+    global_pi.initialized = 1;
+  }
+  for (i=0; i< global_pi.numprocess; i++){
+    print_single_progress(i);
+    printf("\n");
+  }
+}
+
 
 int main(int argc, char **argv) {
     int i;
     double amount = 0.0;
     uint64_t start = get_timestamp();
 
+    init_progress_bars(2);
+    set_progress_bar(0, "\e[33mTest\e[0m", 0.0, start);
+    set_progress_bar(1, "Test2", 0.0, start);
     for (i = 0; i < 10000; i++) {
         amount += 0.01;
-        print_progress(amount, start);
-        usleep(2000000 / (i + 1));
+	update_progress_bar(0, amount);
+        print_multiple_progress();
+        usleep(20000 / (i + 1));
     }
 
     printf("\n");
