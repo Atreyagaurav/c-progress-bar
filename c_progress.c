3001be46c9c0fd87b10d9849176f0c8ef4e44737
Gaurav <allmanpride@gmail.com>
2022-06-02 22:42:52 -0400

    Update readme; Add example

--- c_progress.c
+++ c_progress.c
@@ -1,4 +1,5 @@
 #include "c_progress.h"
+#include <stdio.h>
 
 #ifdef VERTICAL
 static const char * subprogress_blocks[] = { " ",
@@ -195,26 +196,26 @@ void print_line(char *line){
 
 
 int main(int argc, char **argv) {
-    int i;
-    double amount = 0.0;
+  int i, j;
 
-    init_progress_bars(2, 2, 20, 100);
-    start_progress_bar(0, "\e[33mTest\e[0m");
-    start_progress_bar(1, "Test2");
-    for (i = 0; i < 10000; i++) {
-        amount += 0.01;
-	update_progress_bar(0, amount);
-	update_progress_bar(1, amount/2);
-	if (i % 1000 == 0){
-	  print_line("Test Test Test:");
-	  update_status(1, "Hello there");
-	}
-	update_status(0, "Helo there how are you guys right now, are you ok?"
-		      " Do you need some help? Let's see. what do you need"
-		      " right now?");
-        print_multiple_progress();
-        usleep(20000 / (i + 1));
+  init_progress_bars(2, 2, 20, 100);
+  char status1[100], status2[100];
+  start_progress_bar(0, "\e[33mTest\e[0m");
+  start_progress_bar(1, "Test2");
+  for (i = 0; i < 100; i++) {
+    update_progress_bar(1, i*1.0);
+    sprintf(status2, "Process %d of 1000", i+1);
+    update_status(1, status2);
+    if (i % 25 == 0){
+      print_line("LOG: Some text log here.");
     }
 
-    printf("\n");
+    for (j=0; j< 1000; j++){
+      update_progress_bar(0, j/10.0);
+      sprintf(status1, "Subprocess %d of 1000", j+1);
+      update_status(0, status1);
+      print_multiple_progress();
+      usleep(50000 / (i + j + 1));
+    }
+  }
 }
